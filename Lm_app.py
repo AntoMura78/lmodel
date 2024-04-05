3b0a86fd431febc2e75c216b0b9715f79c1fb421
AntoMura78 <163159891+AntoMura78@users.noreply.github.com>
2024-04-05 16:41:34 +0200

    Update Lm_app.py

--- Lm_app.py
+++ Lm_app.py
@@ -24,7 +24,7 @@ openai.api_key = st.secrets["OPkey"]
 
 def askAI(domanda):
     risposta = openai.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4-turbo-preview",
         messages=[
             {
                 "role": "user",
